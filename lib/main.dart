4cc5d0433204b68ccd0581602a6c18e053a34d0d
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-08 17:57:30 +0545

    login with google is successfuly intergrated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/features/auth/pages/home_page.dart';
 import 'package:youtube_clone/features/auth/pages/login_page.dart';
 import 'package:youtube_clone/firebase_options.dart';
 
@@ -25,8 +26,7 @@ class MyApp extends ConsumerWidget {
           if (!snapshot.hasData) {
             return LoginPage();
           }
-          return Container();
-          
+          return HomePage();
         },
       ),
     );
