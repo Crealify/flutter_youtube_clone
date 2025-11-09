24a53f3752dbae3153323ec1be045bacd620b75a
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-09 09:30:03 +0545

    user model to fire store

--- lib/features/auth/repository/user-data_service.dart
+++ lib/features/auth/repository/user-data_service.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:youtube_clone/features/auth/model/user_model.dart';
 
 class UserDataService {
   FirebaseAuth auth;
@@ -10,6 +11,16 @@ class UserDataService {
     required this.firestore,
   });
   addUserDataToFirestore() async {
+    UserModel user = UserModel(
+      displayName: displayName, 
+      username: username, 
+      email: email, 
+      profilePic: profilePic, 
+      subscriptions: subscriptions, 
+      videos: videos, 
+      description: description, 
+      type: type
+      );
 
     
   }
