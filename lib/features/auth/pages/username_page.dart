89514548624c96dcbb26f5aa1133985c2f791a0f
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-16 20:51:24 +0545

    username_page updated: user in users

--- lib/features/auth/pages/username_page.dart
+++ lib/features/auth/pages/username_page.dart
@@ -1,8 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/features/auth/pages/flat_button.dart';
 
-class UsernamePage extends StatelessWidget {
+final formKey = GlobalKey<FormState>();
+
+class UsernamePage extends StatefulWidget {
   const UsernamePage({super.key});
+
+  @override
+  State<UsernamePage> createState() => _UsernamePageState();
+}
+
+class _UsernamePageState extends State<UsernamePage> {
+  final TextEditingController usernameController = TextEditingController();
+  bool isValidate = true;
+
+  void validaeUsername() async {
+    final usersMap = await FirebaseFirestore.instance.collection("user").get();
+    final users = usersMap.docs.map((user) => user).toList();
+    String? targetedUsername;
+
+    for (var user in users) {
+      if (usernameController.text == user.data()['username']) {
+        targetedUsername = user.data()['username'];
+        isValidate = false;
+        setState(() {});
+      }
+      if (usernameController.text != targetedUsername) {
+        isValidate = true;
+        setState(() {});
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,27 +53,31 @@ class UsernamePage extends StatelessWidget {
 
             Padding(
               padding: EdgeInsets.only(right: 15, left: 15),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.verified_user_rounded),
-                  suffixIconColor: Colors.green,
+              child: Form(
+                child: TextFormField(
+                  key: formKey,
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.verified_user_rounded),
+                    suffixIconColor: Colors.green,
 
-                  hintText: "Insert Username",
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green),
+                    hintText: "Insert Username",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green),
+                    ),
                   ),
                 ),
               ),
             ),
 
             const Spacer(),
-            const Padding(
+            Padding(
               padding: const EdgeInsets.only(bottom: 24, right: 15, left: 15),
               child: FlatButton(
                 text: "CONTINUE",
