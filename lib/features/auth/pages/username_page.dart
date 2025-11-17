7718959947cea267600b91334dd970f9f00d2e0a
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-17 14:38:16 +0545

    username_page updated paramaters

--- lib/features/auth/pages/username_page.dart
+++ lib/features/auth/pages/username_page.dart
@@ -1,17 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:youtube_clone/features/auth/pages/flat_button.dart';
+import 'package:youtube_clone/features/auth/repository/user-data_service.dart';
 
 final formKey = GlobalKey<FormState>();
 
-class UsernamePage extends StatefulWidget {
-  const UsernamePage({super.key});
+class UsernamePage extends ConsumerStatefulWidget {
+  final String displayName;
+  final String profilePic;
+  final String email;
+  const UsernamePage({
+    super.key,
+    required this.displayName,
+    required this.profilePic,
+    required this.email,
+  });
 
   @override
-  State<UsernamePage> createState() => _UsernamePageState();
+  ConsumerState<UsernamePage> createState() => _UsernamePageState();
 }
 
-class _UsernamePageState extends State<UsernamePage> {
+class _UsernamePageState extends ConsumerState<UsernamePage> {
   final TextEditingController usernameController = TextEditingController();
   bool isValidate = true;
 
@@ -59,7 +71,9 @@ class _UsernamePageState extends State<UsernamePage> {
                     validaeUsername();
                   },
                   autovalidateMode: AutovalidateMode.always,
-                  validator: (username) {},
+                  validator: (username) {
+                    return isValidate ? null : "Username already taken";
+                  },
                   key: formKey,
                   controller: usernameController,
                   decoration: InputDecoration(
@@ -88,7 +102,24 @@ class _UsernamePageState extends State<UsernamePage> {
               padding: const EdgeInsets.only(bottom: 24, right: 15, left: 15),
               child: FlatButton(
                 text: "CONTINUE",
-                onPressed: () {},
+                onPressed: () async {
+                  isValidate
+                      ? await ref
+                            .read(userDataServiceProvider)
+                            .addUserDataToFirestore(
+                              displayName: widget.displayName,
+                              username: usernameController.text,
+                              email: widget.email,
+                              profilePic: widget.profilePic,
+                              description: "",
+                              subscriptions: [],
+                              videos: 0,
+                              userId: '',
+                              type: '',
+                            )
+                      : null;
+                  // add user data inside databeas
+                },
                 colour: isValidate ? Colors.green : Colors.green.shade100,
               ),
             ),
