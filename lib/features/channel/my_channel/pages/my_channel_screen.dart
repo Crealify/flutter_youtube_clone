e22861fa6c5ab18bce63bcb07eaa6eca41343e08
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-26 15:21:34 +0545

    ConsumerWidget added  to MyChannelScreen

--- lib/features/channel/my_channel/pages/my_channel_screen.dart
+++ lib/features/channel/my_channel/pages/my_channel_screen.dart
@@ -1,39 +1,46 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/cores/screens/error_page.dart';
+import 'package:youtube_clone/cores/screens/loader.dart';
+import 'package:youtube_clone/features/auth/provider/user_provider.dart';
 import 'package:youtube_clone/features/channel/my_channel/parts/bottons.dart';
 import 'package:youtube_clone/features/channel/my_channel/parts/tap_bar.dart';
 import 'package:youtube_clone/features/channel/my_channel/parts/tap_bar_view.dart';
 import 'package:youtube_clone/features/channel/my_channel/parts/top_header.dart';
 
-class MyChannelScreen extends StatelessWidget {
+class MyChannelScreen extends ConsumerWidget {
   const MyChannelScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 7,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: Column(
-              children: [
-                // top header
-                const TopHeader(),
-                const Text(
-                  "More about Crealify!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref
+        .watch(currentUserProvider)
+        .when(
+          data: (currentUser) => DefaultTabController(
+            length: 7,
+            child: const Scaffold(
+              body: SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Column(
+                    children: [
+                      // top header
+                      TopHeader(),
+                      Text("More about Crealify!"),
+                      TapBottons(),
+                      // tab bar
+                      TapBarPage(),
+                      TapViewPages(),
+                    ],
+                  ),
                 ),
-                // bottons
-                const TapBottons(),
-                // tab bar
-                const TapBarPage(),
-                const TapViewPages(),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+          error: (error, stackTrace) => const ErrorPage(),
+          loading: () => const Loader(),
+        );
   }
 }
