1b4ccbe34204662dc0e5bf469f91ddec8ec648e2
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-26 14:55:36 +0545

    splits part : TapBottons

--- lib/features/channel/my_channel/pages/my_channel_screen.dart
+++ lib/features/channel/my_channel/pages/my_channel_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_clone/cores/colors.dart';
-import 'package:youtube_clone/cores/widgets/image_button.dart';
+import 'package:youtube_clone/features/channel/my_channel/parts/bottons.dart';
 import 'package:youtube_clone/features/channel/my_channel/parts/tap_bar.dart';
 import 'package:youtube_clone/features/channel/my_channel/parts/tap_bar_view.dart';
 import 'package:youtube_clone/features/channel/my_channel/parts/top_header.dart';
@@ -25,55 +24,11 @@ class MyChannelScreen extends StatelessWidget {
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.black),
                 ),
-
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 3,
-
-                        child: Container(
-                          height: 40,
-                          decoration: const BoxDecoration(
-                            color: softBlueGreyBackGround,
-                            borderRadius: BorderRadius.all(Radius.circular(9)),
-                          ),
-                          child: TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              "Manage Videos",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                // fontSize: 10,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: ImageButton(
-                          image: "pen.png",
-                          onPressed: () {},
-                          haveColor: true,
-                        ),
-                      ),
-                      Expanded(
-                        child: ImageButton(
-                          image: "time-watched.png",
-                          onPressed: () {},
-                          haveColor: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                // tav bar
+                // bottons
+                const TapBottons(),
+                // tab bar
                 const TapBarPage(),
                 const TapViewPages(),
-
               ],
             ),
           ),
---
+++ lib/features/channel/my_channel/parts/bottons.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/cores/colors.dart';
+import 'package:youtube_clone/cores/widgets/image_button.dart';
+
+class TapBottons extends StatelessWidget {
+  const TapBottons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 3,
+
+            child: Container(
+              height: 40,
+              decoration: const BoxDecoration(
+                color: softBlueGreyBackGround,
+                borderRadius: BorderRadius.all(Radius.circular(9)),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Manage Videos",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    // fontSize: 10,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ImageButton(
+              image: "pen.png",
+              onPressed: () {},
+              haveColor: true,
+            ),
+          ),
+          Expanded(
+            child: ImageButton(
+              image: "time-watched.png",
+              onPressed: () {},
+              haveColor: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
