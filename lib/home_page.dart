7a3ef97c0af6352b8c6e1c67fac04c57d8ecd8c3
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-28 21:53:03 +0545

    else condition           showModalBottomSheet added to haomepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,7 @@ import 'package:youtube_clone/cores/screens/loader.dart';
 import 'package:youtube_clone/cores/widgets/image_button.dart';
 import 'package:youtube_clone/features/auth/provider/user_provider.dart';
 import 'package:youtube_clone/features/content/bottom_navigation.dart';
+import 'package:youtube_clone/features/upload/upload_bottom_sheets.dart';
 import 'package:youtube_clone/pages_list.dart';
 
 class HomePage extends StatefulWidget {
@@ -92,6 +93,11 @@ class _HomePageState extends State<HomePage> {
           if (index != 2) {
             currentIndex = index;
             setState(() {});
+          } else {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) => CreateBottomSheet(),
+            );
           }
         },
       ),
