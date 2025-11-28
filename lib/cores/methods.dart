8f0dab70f0ab0bd5899cb273ff4ccee5ff5a384b
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-28 22:12:22 +0545

    image picker dependency added

--- lib/cores/methods.dart
+++ lib/cores/methods.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: unnecessary_null_comparison, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showErrorSnackBar(String message, context) =>
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text(message), duration: const Duration(seconds: 1)),
     );
+pickvideo() async {
+  XFile? file = await ImagePicker().pickVideo(source: ImageSource.gallery);
+}
