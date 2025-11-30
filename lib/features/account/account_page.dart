753d3f0f0fa017468e5e267f3ba82aa266c09842
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-30 15:39:32 +0545

    putting actual usermodel data on teh accountPage

--- lib/features/account/account_page.dart
+++ lib/features/account/account_page.dart
@@ -1,15 +1,45 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
 import 'package:youtube_clone/features/account/items.dart';
+import 'package:youtube_clone/features/auth/model/user_model.dart';
 
 class AccountPage extends StatelessWidget {
-  const AccountPage({super.key});
+  final UserModel user;
+  const AccountPage({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: SafeArea(child: Column(children: [Items()])),
+        padding: const EdgeInsets.only(top: 20),
+        child: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.grey,
+                backgroundImage: CachedNetworkImageProvider(user.profilePic),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 12),
+                child: Text(
+                  user.displayName,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Text(
+                  user.username,
+                  style: TextStyle(fontSize: 16, color: Colors.blueGrey),
+                ),
+              ),
+              const Items(),
+            ],
+          ),
+        ),
       ),
     );
   }
