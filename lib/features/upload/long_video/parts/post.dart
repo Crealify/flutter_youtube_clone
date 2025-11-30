0cfa3be8355d9c710922cf9a7708a2598bc00bce
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-11-30 20:51:09 +0545

    flat button. video.dart, videoExternalbutton are fetched

--- lib/features/upload/long_video/parts/post.dart
+++ lib/features/upload/long_video/parts/post.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youtube_clone/features/auth/model/user_model.dart';
 import 'package:youtube_clone/features/auth/provider/user_provider.dart';
+import 'package:youtube_clone/features/upload/long_video/parts/video.dart';
 
 import 'package:youtube_clone/features/upload/long_video/video_model.dart';
 
@@ -19,59 +20,62 @@ class Post extends ConsumerWidget {
     userModel.whenData((user) => user);
 
     final user = userModel.whenData((user) => user);
-    return Column(
-      children: [
-        CachedNetworkImage(imageUrl: video.thumbnail),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, left: 5),
-              child: CircleAvatar(
-                radius: 20,
-                backgroundColor: Colors.grey,
-                backgroundImage: CachedNetworkImageProvider(
-                  user.value!.profilePic,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                video.title,
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            const Spacer(),
-            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-          ],
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-            left: MediaQuery.sizeOf(context).width * 0.14,
-          ),
-          child: Row(
+    return GestureDetector(
+      onTap:(){ Navigator.push(context, MaterialPageRoute(builder: (context)=> const Video()));},
+      child: Column(
+        children: [
+          CachedNetworkImage(imageUrl: video.thumbnail),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+      
             children: [
-              Text(
-                user.value!.displayName,
-                style: TextStyle(color: Colors.blueGrey),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, left: 5),
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: CachedNetworkImageProvider(
+                    user.value!.profilePic,
+                  ),
+                ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
+                padding: const EdgeInsets.only(left: 10),
                 child: Text(
-                  video.views == 0 ? "No View" : "${video.views}",
-                  style: const TextStyle(color: Colors.blueGrey),
+                  video.title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
-              const Text(
-                "a moment ago",
-                style: TextStyle(color: Colors.blueGrey),
-              ),
+              const Spacer(),
+              IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
             ],
           ),
-        ),
-      ],
+          Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.sizeOf(context).width * 0.14,
+            ),
+            child: Row(
+              children: [
+                Text(
+                  user.value!.displayName,
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Text(
+                    video.views == 0 ? "No View" : "${video.views}",
+                    style: const TextStyle(color: Colors.blueGrey),
+                  ),
+                ),
+                const Text(
+                  "a moment ago",
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
