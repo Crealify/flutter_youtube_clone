151ddd505da0f7df8f7126adaad3db5fed76db2f
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-02 14:31:19 +0545

    user can now able to trim vidoeo long as user requireed

--- lib/features/upload/short_video/widget/trim_slinder.dart
+++ lib/features/upload/short_video/widget/trim_slinder.dart
@@ -5,7 +5,7 @@ import 'package:video_editor/video_editor.dart';
 String formatter(Duration duration) => [
   duration.inMinutes.remainder(60).toString().padLeft(2, "0"),
   duration.inSeconds.remainder(60).toString().padLeft(2, "0"),
-].join("");
+].join(":");
 
 class MyTrimSlider extends StatefulWidget {
   final VideoEditorController controller;
