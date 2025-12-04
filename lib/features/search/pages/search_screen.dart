94cd4e32429d1cbb61671edf3b3a6b12c6f8f1cc
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-04 09:34:39 +0545

    listview builder logic addes .

--- lib/features/search/pages/search_screen.dart
+++ lib/features/search/pages/search_screen.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youtube_clone/cores/widgets/custom_botton.dart';
+import 'package:youtube_clone/features/auth/model/user_model.dart';
+import 'package:youtube_clone/features/search/provider/search_provider.dart';
 import 'package:youtube_clone/features/search/widgets/search_channel_tile.dart';
+import 'package:youtube_clone/features/upload/long_video/parts/post.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends ConsumerStatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  ConsumerState<SearchScreen> createState() => _SearchScreenState();
+}
+
+//Core Logic
+
+class _SearchScreenState extends ConsumerState<SearchScreen> {
+  List foundItems = [];
+
+  filterList(String keyWordSelected) async {
+    List<UserModel> users = await ref.watch(allChannelsProvider);
+    List result = [];
+    final foundChannels = users.where((user) {
+      return user.displayName.toString().toLowerCase().contains(
+        keyWordSelected,
+      );
+    }).toList();
+    result.addAll(foundChannels);
+    final foundVideos = users.where((video) {
+      return video.displayName.toString().toLowerCase().contains(
+        keyWordSelected,
+      );
+    }).toList();
+    result.addAll(foundVideos);
+
+    setState(() {
+      result.shuffle();
+      foundItems = result;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,6 +82,25 @@ class SearchScreen extends StatelessWidget {
                 ],
               ),
               SearchChannelTile(),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: foundItems.length,
+                  itemBuilder: (context, index) {
+                    List<Widget> itemWidgets = [];
+                    final selectedItem = foundItems[index];
+
+                    if (selectedItem.type == "video") {
+                      itemWidgets.add(Post(video: selectedItem));
+                    } else if (selectedItem.type == "user") {
+                      itemWidgets.add(SearchChannelTile());
+                    } else if (foundItems.isEmpty) {
+                      return const SizedBox();
+                    }
+                    return itemWidgets[0];
+                  },
+                ),
+              ),
             ],
           ),
         ),
