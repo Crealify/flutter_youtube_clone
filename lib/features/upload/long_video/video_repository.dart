068d021c3fe52ed47fa6431fb7639f4caf3773a7
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-05 21:32:32 +0545

    arrayUnion and arrayRemove update for like and unlikes video login added

--- lib/features/upload/long_video/video_repository.dart
+++ lib/features/upload/long_video/video_repository.dart
@@ -33,10 +33,18 @@ class VideoRepository {
     await firestore.collection("videos").doc(videoId).set(video.toMap());
   }
 
-  likeVideo({likes, videoId, currentUserId}) async {
-    FirebaseFirestore.instance.collection("videos").doc(videoId).update({
-      // arrayUnion is used to modified and update field documents
-      "likes": FieldValue.arrayUnion([currentUserId]),
-    });
+  likeVideo({List? likes, videoId, currentUserId}) async {
+    if (!likes!.contains(currentUserId)) {
+      FirebaseFirestore.instance.collection("videos").doc(videoId).update({
+        // arrayUnion is used to modified and update field documents
+        "likes": FieldValue.arrayUnion([currentUserId]),
+      });
+    }
+    if (likes.contains(currentUserId)) {
+      FirebaseFirestore.instance.collection("videos").doc(videoId).update({
+        // arrayUnion is used to modified and update field documents
+        "likes": FieldValue.arrayRemove([currentUserId]),
+      });
+    }
   }
 }
